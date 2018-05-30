dfb48a8ef152a2021e42b980209e83017f11f0dd
Vinicius Ribeiro <39604888+viniciusrsss@users.noreply.github.com>
2018-05-30 00:46:56 -0300

    Só o de mostrar a tabela que é bestinha
    Só precisa trocar o '1' por new_line e qnt_colun

---
+++ voidmostrartabela.c
@@ -0,0 +1,15 @@
+void mostrartabela(char **table)
+{
+  int i;
+  //new_line
+  int j;
+  //qnt_collun
+
+  for ( i = 0; i < 1; i++)
+  {
+   for ( j = 0; j < 1; j++ )
+    {
+     printf("%s \n", &table[i][j]); 
+    }
+  }
+}
