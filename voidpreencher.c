92b239403e1649879fff1d7e0a40b6dbd5b6b7a4
Vinicius Ribeiro <39604888+viniciusrsss@users.noreply.github.com>
2018-06-04 21:40:34 -0300

    Update voidpreencher.c

--- voidpreencher.c
+++ voidpreencher.c
@@ -2,54 +2,24 @@
 #include <string.h>
 #include <stdlib.h>
 
-void insetirintens(char **table)
+void insetirintens(int new_line, int qdt_colun, char ***table)
 {
-    //Valores inteiros a gente troca por new_line e qnt_colun no malloc - Vinicius
-    //Dentro dos 'for' tambem - Vinicius
-    int i;
-    int j;
-    
-    for (i = 0; i < 1; i++)
-    {
-        for (j = 0; j < 1; j++ )
-        {
-            scanf(" %s", &table[i][j]);
-        }
-    }   
-}
-
-int main()
-{
-  char **table;
   int i;
-  //new_line
   int j;
-  //qnt_colun
-  
-  table =  malloc(100 * sizeof(char));
- 
-  for (i = 0; i < 1; i++ )
-  {
-    table[i] =  malloc(100 * sizeof(char));
-  }
 
-  //Essa parte de alocar roubei do teu main pra ficar igualzinho - Vinicius
-  //Mas basta trocar 100 pelo new_line e qnt_colun - Vinicius
+  table = (char***) malloc(50 * sizeof(char **));
     
-  insetirintens(table); //Chamamos o preenchimento
-    
-  for ( i = 0; i < 1; i++)
+  for (i = 0; i < new_line; i++)
   {
-   for ( j = 0; j < 1; j++ )
+    table[i] = (char**) malloc(50 * sizeof(char*));
+    for (j = 0; j < qdt_colun; j++)
     {
-     printf("%s \n", &table[i][j]); 
-     //Printamos na tela 
-     //Essa parte da para aproveitar na função listagem de itens
+      printf("Pode Digitar a informação do indice [%d][%d]: \n", i, j);
+      table[i][j] = (char*) malloc(50 * sizeof(char));
+      scanf(" %s", table[i][j]);
+      printf("Mostrando informação do indice [%d][%d]\n", i, j);
+      printf("%s\n", table[i][j]); 
     }
-  }
-
-  //!!!Tem só algum role troncho que numa 2x2 ele mistura algumas informações - Vinicius
-  //A gente podia tirar essa duvida com Erick
-    
- return 0;
+  }   
 }
+
